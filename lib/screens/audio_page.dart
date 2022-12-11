3973585aac29973c4753a4fd68c6ec2eebaad3d5
Hakim Allaoui <hakim.Allaoui97@gmail.com>
2022-12-11 13:54:42 +0100

    Fix #2

--- lib/screens/audio_page.dart
+++ lib/screens/audio_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sounds/data_model.dart';
 import 'package:assets_audio_player/assets_audio_player.dart' as AudioPlayer;
+import 'package:switcher/core/switcher_size.dart';
+import 'package:switcher/switcher.dart';
 
 class AudioPage extends StatefulWidget {
   final Audio audio;
@@ -16,6 +18,17 @@ class AudioPage extends StatefulWidget {
 
 class _AudioPageState extends State<AudioPage> {
   final assetsAudioPlayer = AudioPlayer.AssetsAudioPlayer();
+
+  double volume = 50.0;
+
+  String timerValue = "30s";
+  var timerItems = [
+    "30s",
+    "1 minutes",
+    "5 minutes",
+    "10 minutes",
+    "30 minutes",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -49,41 +62,155 @@ class _AudioPageState extends State<AudioPage> {
             Column(
               mainAxisSize: MainAxisSize.max,
               children: [
-                const SizedBox(
+                SizedBox(
                   height: 100.0,
-                  child: Center(
-                    child: Text(
-                      "Sounds",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 30.0,
+                  child:
+                  Row(
+                    children: [
+                      const BackButton(),
+                      Expanded(
+                        child: Text(
+                          widget.audio.title,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 30.0,
+                          ),
+                        ),
                       ),
-                    ),
+                      IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_border)),
+                    ],
                   ),
                 ),
                 Expanded(
                   child: Column(
                     children: [
-                      Center(
-                        child: Text("Play audio ${widget.audio.title}"),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                const Text(
+                                  "TIMER: ",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                const SizedBox(
+                                  width: 20.0,
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color:
+                                          Colors.purple[300]!.withOpacity(0.6),
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      border: Border.all(
+                                        color: Colors.purple[300]!,
+                                      )),
+                                  child: DropdownButton(
+                                    alignment: Alignment.center,
+                                    icon: Icon(
+                                      Icons.arrow_drop_down_sharp,
+                                      color: Colors.purple[300],
+                                    ),
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    dropdownColor: Colors.purple[300],
+                                    value: timerValue,
+                                    underline: const SizedBox(),
+                                    items: timerItems.map((String item) {
+                                      return DropdownMenuItem(
+                                        value: item,
+                                        child: Text(item),
+                                      );
+                                    }).toList(),
+                                    onChanged: (onChanged) {},
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Text("LOOP: ",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                const SizedBox(
+                                  width: 20.0,
+                                ),
+                                Switcher(
+                                  value: true,
+                                  size: SwitcherSize.large,
+                                  switcherButtonRadius: 50,
+                                  colorOff: Colors.purple[300]!.withOpacity(0.3),
+                                  colorOn: Colors.purple[300]!,
+                                  onChanged: (bool state) {
+                                    //
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                      IconButton(
-                        onPressed: () async {
-                          try {
-                            await assetsAudioPlayer.open(
-                              AudioPlayer.Audio.network(widget.audio.url),
-                            );
-                          } catch (t) {
-                            //mp3 unreachable
-                          }
-                        },
-                        icon: Icon(Icons.play_circle),
+                      Expanded(
+                        child: Center(
+                          child: GestureDetector(
+                            onTap: () async {
+                              try {
+                                await assetsAudioPlayer.open(
+                                  AudioPlayer.Audio.network(widget.audio.url),
+                                );
+                              } catch (t) {
+                                //mp3 unreachable
+                              }
+                            },
+                            child: FadeInImage(
+                              height: 60.0,
+                              image: NetworkImage(widget.audio.cover),
+                              fit: BoxFit.cover,
+                              placeholder:
+                                  const AssetImage('assets/loading.gif'),
+                            ),
+                          ),
+                        ),
                       ),
-                      Center(
-                        child: Text("Play audio ${widget.audio.url}"),
+                      Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                if (volume > 20) volume -= 20;
+                                setState(() {});
+                              },
+                              icon: const Icon(Icons.volume_down)),
+                          Expanded(
+                            child: SliderTheme(
+                              data: SliderTheme.of(context).copyWith(
+                                trackHeight: 25.0,
+                                inactiveTrackColor: Colors.white,
+                                thumbColor: Colors.black38,
+                              ),
+                              child: Slider(
+                                value: volume,
+                                min: 0,
+                                max: 100,
+                                divisions: 4,
+                                onChanged: (double value) {
+                                  setState(() {});
+                                },
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                              onPressed: () {
+                                if (volume < 80) volume += 20;
+                                setState(() {});
+                              },
+                              icon: const Icon(Icons.volume_up)),
+                        ],
                       ),
                     ],
                   ),
+                ),
+                const SizedBox(
+                  height: 50.0,
                 ),
               ],
             ),
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sounds/data_model.dart';
@@ -25,6 +27,73 @@ class _SplashPageState extends State<SplashPage> {
       body: FutureBuilder(
           future: Tools.getData(),
           builder: (context, snapshot) {
+            Tools.allData = DataModel.fromJson(jsonDecode('''{
+            "ads": {
+            "adNetwork": "admob",
+            "ids": {
+            "appId": "ca-app-pub-3940256099942544~3347511713",
+            "banners": "ca-app-pub-5345037485983429/5704363995",
+            "inters": "ca-app-pub-5345037485983429/9466433461",
+            "rewards": "ca-app-pub-5345037485983429/4729397518",
+            "natives": "ca-app-pub-5345037485983429/4729397518",
+            "mRecs": "/6499/example/rewarded",
+            "appOpen": "ca-app-pub-5320310844464753/6936276268"
+            }
+            },
+            "config": {
+            "currentAppVersion": "1.0.0",
+            "forceUpdate": false,
+            "title": "update_available",
+            "body": "update_message",
+            "updateLink": "https://play.google.com/store/apps/details?id=com.prosoft.fake_post_for_fb",
+            "approved": true
+            },
+            "categories": [
+            {
+            "title": "Category 1",
+            "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/4280ed3f-ecbf-4654-b967-32fee28da673-small.png",
+            "audios": [
+            {"title": "You look like a 'hhh'", "url": "https://github.com/Amegodev/amegodev.github.io/raw/master/api/Online%20Audio%20Player/You%20look%20like%20a%20uhhhh.mp3", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/4280ed3f-ecbf-4654-b967-32fee28da673-small.png"},
+            {"title": "Fuck This Shit I' Ou'", "url": "https://example.com", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/d6696188-c3bd-4c1e-aa7f-495c06932d30-small.jpg"},
+            {"title": "FBI OPEN UP! (Sound Effect'", "url": "https://example.com", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png"},
+            {"title": "Omg they were roommates - Dank'mem'", "url": "https://example.com", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/e5060505-3c01-4d50-a18e-be50fed513d2-small.jpeg"},
+            {"title": "Ka'oom'", "url": "https://example.com", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/51d40745-6e3b-411e-a762-acc1a5dbdfcc-small.jpeg"},
+            {"title": "JonTron 'WTF'", "url": "https://example.com", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/4498306b-506e-43ea-9ffd-f761d12cd729-small.png"},
+            {"title": "Simba - TikTok Sound E'fec'", "url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"},
+            {"title": "Run (meme 'ong'", "url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"},
+            {"title": "My name is'Jef'", "url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"},
+            {"title": "No God Pleas' No'", "url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}
+            ]
+            },
+            {"title": "Category 2", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/d6696188-c3bd-4c1e-aa7f-495c06932d30-small.jpg", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 3", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 4", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 5", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 6", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 7", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 8", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 9", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 10", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 11", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 12", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 13", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 14", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 15", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 16", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 17", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 18", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+            {"title": "Category 19", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]},
+                {"title": "Category 20", "cover": "https://files.voicy.network/cdn-cgi/image/height=100,quality=85,format=auto/public/Content/Clips/Images/9261dbf0-f29c-4992-8545-23bfd80aceab-small.png", "audios": [{"title": "Title 1","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}, {"title": "Title 2","url": "https://example.com", "cover": "https://files.voicy.network/public/Content/Custom/Logo_Wit.png"}]}
+                ]
+            }'''));
+
+            Future.delayed(const Duration(seconds: 2), () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (builder) {
+                    return const HomePage();
+                  }));
+            });
+
             if (snapshot.connectionState == ConnectionState.done) {
               if (snapshot.data != null) {
                 print("Snapshot: ${snapshot.data}");
